f185afcd5890cf6a0b03d5fa23073b9b75e7c63c
Natalie Sokolova <nataliesklv@gmail.com>
2021-01-19 21:58:33 -0500

    maps over data and shows it on the frontend

--- client/src/components/Movement.jsx
+++ client/src/components/Movement.jsx
@@ -9,6 +9,14 @@ export default function Movement() {
       {console.log("STATE: ", state.movements)}
       <div>Movements:</div>
       <br />
+      {state.movements.map((movement) => (
+        <div key={movement.description}>
+          <div>Start Location: {movement.start}</div>
+          <div>End Location: {movement.end}</div>
+          <div>Description: {movement.description}</div>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
