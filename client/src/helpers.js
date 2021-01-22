d5371dabe16e1db2d0d8ab84ff074fa4e2fbf91a
Natalie Sokolova <nataliesklv@gmail.com>
2021-01-22 15:40:43 -0500

    refactors isDuplicate helper to use arr.find instead arr.forEach

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,21 +1,11 @@
 // checks is a movement already exists
 const isDuplicate = (obj, arr) => {
-  let result = false;
-  // change forEach to find
-  // remove booleans and if stat.
-  // return arr.find
-
-  arr.forEach((movementObj) => {
-    if (
-      obj.start === movementObj.start &&
-      obj.end === movementObj.end &&
-      obj.description === movementObj.description
-    ) {
-      result = true;
-    }
-  });
-  // console.log("RES: ", result);
-  return result;
+  return arr.find(
+    (element) =>
+      obj.start === element.start &&
+      obj.end === element.end &&
+      obj.description === element.description
+  );
 };
 
 const isFilledOut = (obj) => {
