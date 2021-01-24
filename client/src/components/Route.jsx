59d8ebdfe24473c98b574752594e086154c0799e
Natalie Sokolova <nataliesklv@gmail.com>
2021-01-23 20:37:58 -0500

    finds closest location by coordinates

--- client/src/components/Route.jsx
+++ client/src/components/Route.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import "./Route.css";
 
 export default function Route({ movements, route, setRoute }) {
-  // console.log("MV: ", movements);
-
   const findClosestLocation = (startingLocation, locationData) => {
     const vectorDistance = (dx, dy) => {
       return Math.sqrt(dx * dx + dy * dy);
     };
 
     const locationDistance = (location1, location2) => {
-      const dx = location1.latitude - location2.latitude,
-        dy = location1.longitude - location2.longitude;
+      const dx = location1[0] - location2[0],
+        dy = location1[1] - location2[1];
 
       return vectorDistance(dx, dy);
     };
@@ -19,25 +17,57 @@ export default function Route({ movements, route, setRoute }) {
     return locationData.reduce((prev, curr) => {
       const prevDistance = locationDistance(startingLocation, prev),
         currDistance = locationDistance(startingLocation, curr);
-      console.log(prevDistance < currDistance ? prev : curr);
       return prevDistance < currDistance ? prev : curr;
     });
   };
 
-  let startingLocation = {
-    latitude: 43.65107,
-    longitude: -79.347015,
+  let startingLocation = [29.9511, -90.0715];
+
+  const checkRoute = (route, coordinates) => {
+    return route.some((city) =>
+      coordinates.every((position, index) => position === city[index])
+    );
+  };
+
+  const generateCoordList = () => {
+    let cities = [];
+    // loops over movements
+    movements.forEach((movement, index) => {
+      const startCoordinates = [
+        Number(movement.startLat),
+        Number(movement.startLong),
+      ];
+      // const endCoordinates = [
+      //   Number(movement.endLat),
+      //   Number(movement.endLong),
+      // ];
+
+      // check if starting city coordinates are present in a route array
+      // some() tests whether at least one city passes check defined in every()
+      // every() tests whether lat and long are present in cities
+      if (!checkRoute(cities, startCoordinates)) {
+        // if not => push city into cities
+        cities.push(startCoordinates);
+      }
+    });
+    console.log("CITIES: ", cities);
+    return cities;
+  };
+
+  const generateRoute = () => {
+    const list = generateCoordList();
+    let closest = findClosestLocation(startingLocation, list);
+
+    console.log("CLOSEST: ", closest);
   };
 
   return (
     <div id="route">
-      {/* <button onClick={generateRoute} type="submit"> */}
       <div className="btn-group">
         <button
           type="button"
           className="btn btn-success"
-          // onClick={generateRoute}
-          onClick={() => findClosestLocation(startingLocation, movements)}
+          onClick={generateRoute}
         >
           Generate Route
         </button>
