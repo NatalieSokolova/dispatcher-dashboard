c15943955b0cc8f3ac0c1a595a96988aca5b5683
Natalie Sokolova <nataliesklv@gmail.com>
2021-02-09 20:54:17 -0500

    pseudocodes new solution for generating driver's route

--- client/src/components/Route.jsx
+++ client/src/components/Route.jsx
@@ -16,6 +16,14 @@ export default function Route({ movements, route, setRoute, setMapData }) {
     return [setRoute(generateRoute()), setMapData("Route")];
   };
 
+  // REFACTOR:
+  // on first iteration of the loop find movement with closest starting location
+  // after first iteration, check if end coords of the same movement are closer than the closest starting coords of a new movement
+  // after visiting each city's starting coords add that city's end coords to the arr
+  // after visiting each location remove it from the arr to avoid visiting the same location twice
+  // change loop's stopping condition (because locationStartCoords.length will be changing inside the loop)
+  // when done => move logic to the backend (post route for driverRoute?)
+
   const generateRoute = () => {
     let generatedRoute = [];
     // creates a list of movements' starting coordinates
